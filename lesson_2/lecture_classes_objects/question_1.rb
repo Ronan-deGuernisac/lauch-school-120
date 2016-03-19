59d523ff9fcb591b6ae75368894097ad1026a8b4
superchilled <karl.lingiah@gmail.com>
2016-03-19 19:57:59 +0000

    Adding Lesson 2 files

---
+++ lesson_2/lecture_classes_objects/question_1.rb
@@ -0,0 +1,16 @@
+# question_1.rb
+
+class Person
+  attr_accessor :name
+  
+  def initialize(name)
+  
+  @name = name
+  
+  end
+end
+
+bob = Person.new('bob')
+puts bob.name                  # => 'bob'
+bob.name = 'Robert'
+puts bob.name                  # => 'Robert'
